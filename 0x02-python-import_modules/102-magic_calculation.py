d35ff6aef6bfb3ecd7d1e6ca95ad340c416684f1
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-04 17:18:50 +0100

    corrections

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_102_helper import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         res = add(a, b)
         for k in range(4, 6):
