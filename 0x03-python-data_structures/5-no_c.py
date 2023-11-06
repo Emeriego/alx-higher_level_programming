fc069c9931d37792d0ae5e5daaf7107105f03b9b
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-06 18:51:14 +0100

    fifth task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     i = 0
     for ch in my_string:
-        if ch == 'c' and ch == 'C':
+        if ch == 'c' or ch == 'C':
             del my_string[i]
         i = i + 1
     return my_string
