0ed28639dbdc509fac5df77a86dc57a31d68710e
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-21 02:33:18 +0100

    correctons

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    sum = 0
+    c = 0
+
     for item in range(x):
         try:
-            print(f"{my_list[item]}", end="")
-            sum += 1
-
-        except IndexError:
+            print("{}".format(my_list[item]), end='')
+        except:
             break
+        else:
+            c += 1
 
     print()
-    return(sum)
+    return (c)
