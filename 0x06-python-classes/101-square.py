8e493835a04675fc0f91902a4e2fdf52949e5dd8
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-22 13:04:08 +0100

    corrections

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -72,7 +72,7 @@ class Square:
         """
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
+        for item in range(0, self.__size):
             [print(" ", end="") for m in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
 
