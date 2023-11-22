493234ccfb35bb299030262dcaaeef06a0b8d6a2
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-22 05:39:20 +0100

    corrections

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,8 +39,8 @@ class Square:
     def my_print(self):
         """Prints square with a # character."""
 
-        if self.__size == 0:
-            print("")
-        for item in range(0, self.__size):
+        for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
             print("")
