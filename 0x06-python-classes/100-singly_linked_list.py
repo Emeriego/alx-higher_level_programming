f8c33c424833026741bad0c08c042456063577b8
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-22 13:02:04 +0100

    corrections

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Task7 Defines classes for an S_list."""
+"""Define classes for a singly-linked list."""
 
 
 class Node:
-    """Class Represent a node in an S_list.
-    """
+    """Represent a node in a singly-linked list."""
 
     def __init__(self, data, next_node=None):
-        """Initializes a new Node.
+        """Initialize a new Node.
+
         Args:
-            data (int): Data of new Node.
+            data (int): The data of the new Node.
             next_node (Node): The next node of the new Node.
         """
-        self.data = datasss
+        self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Gets the data of Node.
-        """
+        """Get/set the data of the Node."""
         return (self.__data)
 
     @data.setter
@@ -29,13 +28,10 @@ class Node:
 
     @property
     def next_node(self):
-        """Gets the next node.
-        """
+        """Get/set the next_node of the Node."""
         return (self.__next_node)
 
     @next_node.setter
-        """Set the next node.
-        """
     def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
@@ -43,18 +39,20 @@ class Node:
 
 
 class SinglyLinkedList:
-    """Class Represents an S_list.
-    """
+    """Represent a singly-linked list."""
 
     def __init__(self):
-        """Initalizes new List.
-        """
+        """Initalize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
-        """Class to Insert a new Node to List.
+        """Insert a new Node to the SinglyLinkedList
+
+        The node is inserted into the list at the correct
+        ordered numerical position.
+
         Args:
-            value (Node): Node to be inserted.
+            value (Node): Enter the new Node to insert.
         """
         new = Node(value)
         if self.__head is None:
@@ -64,19 +62,18 @@ class SinglyLinkedList:
             new.next_node = self.__head
             self.__head = new
         else:
-            curr = self.__head
-            while (curr.next_node is not None and
-                    curr.next_node.data < value):
-                curr = curr.next_node
-            new.next_node = curr.next_node
-            curr.next_node = new
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
 
     def __str__(self):
-        """method Defines the print() representation
-        """
+        """Define the print() representation of a SinglyLinkedList."""
         values = []
-        curr = self.__head
-        while curr is not None:
-            values.append(str(curr.data))
-            curr = curr.next_node
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
         return ('\n'.join(values))
