f55e65bd77fbf3174c610ede312591f764353b97
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-28 03:44:03 +0100

    more OOP

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -43,8 +43,8 @@ class Rectangle:
         that is able to recreate a new instance by using eval() is returned
 
         Returns:
-            Returns the official string
-        """        
+        Returns the official string
+        """
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     @property
