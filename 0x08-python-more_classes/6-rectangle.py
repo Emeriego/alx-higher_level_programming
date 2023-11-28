f77a4be53d993eeb6b1fc396e2ea5eb79676a5b2
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-28 04:07:49 +0100

    more OOP

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,7 +6,7 @@ The class reprrsents a Rectangle.
 
 class Rectangle:
     """This class is defined by width and height.
-    
+
     Attributes:
         number_of_instances: number of Rectangle instances,
         increments on every instance,
@@ -27,7 +27,8 @@ class Rectangle:
         Rectangle.number_of_instances += 1
 
     def __str__(self):
-        """Method Returns an informal and nicely printable string representation
+        """Method Returns an informal and nicely
+        printable string representation
         of a Rectangle instance usin '#'."""
         if self.__height == 0 or self.__width == 0:
             return ''
@@ -48,7 +49,6 @@ class Rectangle:
         """Method Deletes an instance. using 'Del instance()'"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
 
     @property
     def width(self):
@@ -86,7 +86,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
 
     def area(self):
         """Uses the params to find the area of Rectangle instance
