9bdaf923a74edd14d13dd8a5d1efbe9dd313dfb9
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-06 22:30:56 +0100

    corrections

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Module Name: 11-square, alternates == and !="""
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class MyInt(int):
-    """Subclass of global class int"""
-    def __eq__(self, other):
-        """alternates == and != """
-        return int(self) != other
+class Square(Rectangle):
+    """Represent a square."""
 
-    def __ne__(self, other):
-        """alternates == and !="""
-        return int(self) == other
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
