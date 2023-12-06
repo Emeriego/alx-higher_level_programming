ea5e3a7f6ab81c392fef47682ecbf7acd6072881
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-06 22:36:38 +0100

    corrections

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,11 +10,11 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
+    txt = ""
+    with open(filename) as hr:
+        for line in hr:
+            txt += line
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+                txt += new_string
+    with open(filename, "w") as hw:
+        hw.write(txt)
