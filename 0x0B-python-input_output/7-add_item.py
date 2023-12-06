2806c7f0dc5cd241f535db333d8780b05efe2842
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-06 01:55:08 +0100

    file operations

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Module adds all arguments to a
-Python list and save them to a file."""
+"""Module add all arguments to a Python
+list and save them to a file."""
 import sys
+import os
+
 
 if __name__ == "__main__":
+
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        stuffs = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        stuffs = []
-    stuffs.extend(sys.argv[1:])
-    save_to_json_file(stuffs, "add_item.json")
+    if os.path.isfile("add_item.json"):
+        stuff = load_from_json_file("add_item.json")
+    else:
+        stuff = []
+
+    save_to_json_file(stuff + sys.argv[1:], "add_item.json")
