b4aec8dd64f1f83e31428819371fdc8330893e51
Godwin <emeriego@gmail.com>
2024-01-17 16:31:42 -0800

    corrections on corrections

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
 -- Script Lists all cities of CA in the database hbtn_0d_usa.
 SELECT cities.name
 FROM cities
-WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California')
+WHERE cities.state_id = (SELECT states.id
+	FROM states
+	WHERE states.name = 'California')
 ORDER BY cities.id ASC;
