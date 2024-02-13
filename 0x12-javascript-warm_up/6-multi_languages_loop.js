ba455e836541410957038ca7f0e374c48d0085e4
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-02-13 11:00:54 +0100

    semicolon

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const sent = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const sent = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (line of sent) {
   console.log(line);
 }
